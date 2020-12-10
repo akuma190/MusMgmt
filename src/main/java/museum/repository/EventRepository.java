5a57e240d1fb55120fa7e0a39daceb2f48bc9312
Ashish Mishra <ashishk861@gmail.com>
2020-12-09 21:17:55 -0500

    Update EventRepository.java

--- src/main/java/museum/repository/EventRepository.java
+++ src/main/java/museum/repository/EventRepository.java
@@ -12,17 +12,19 @@ import java.util.List;
 @Repository
 public interface EventRepository extends CrudRepository<event,Integer> {
 
-    @Query(value="select ev.event_id,\n" +
-            "       ev.event_name,\n" +
-            "\t   ev.event_type,\n" +
-            "\t   ev.creation_date,\n" +
-            "\t   ev.artist_id,\n" +
-            "       count(*) as painting_count\n" +
-            "       from museumdbms.event ev,\n" +
-            "            museumdbms.event_artwork er\n" +
-            "\t   where ev.event_id=er.event_id\n" +
-            "       and er.status='in_event'\n" +
-            "       group by ev.event_id",nativeQuery=true)
+//    @Query(value="select ev.event_id,\n" +
+//            "       ev.event_name,\n" +
+//            "\t   ev.event_type,\n" +
+//            "\t   ev.creation_date,\n" +
+//            "\t   ev.artist_id,\n" +
+//            "       count(*) as painting_count\n" +
+//            "       from museumdbms.event ev,\n" +
+//            "            museumdbms.event_artwork er\n" +
+//            "\t   where ev.event_id=er.event_id\n" +
+//            "       and er.status='in_event'\n" +
+//            "       group by ev.event_id",nativeQuery=true)
+//    List<event> findByEventId();
+    @Query(value="select * from event",nativeQuery=true)
     List<event> findByEventId();
 
     @Query(value="select count(*) as painting_count\n" +
@@ -32,7 +34,7 @@ public interface EventRepository extends CrudRepository<event,Integer> {
             "\t   and ev.event_id=:eventId\n" +
             "       and er.status='in_event'\n" +
             "       group by ev.event_id",nativeQuery=true)
-    int findCountById(@Param("eventId") Integer eventId);
+    Integer findCountById(@Param("eventId") Integer eventId);
 
     @Query(value="select * from event where event_id =:eventId",nativeQuery=true)
     event findOne(@Param("eventId") Integer eventId);
